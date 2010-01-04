facf745d63aa0ac256b3419f18675040ec6ccdff
daniel <git@daniel-nilsson.com>
2010-01-04 22:48:47 +0100

    Improve ExitShop my moving to the door.

--- lib/TAEB/AI/Behavioral/Behavior/ExitShop.pm
+++ lib/TAEB/AI/Behavioral/Behavior/ExitShop.pm
@@ -7,8 +7,30 @@ sub prepare {
 
     return unless TAEB->current_tile->in_shop;
 
-    # look for a tile with a shopkeeper nearby
     my $path = TAEB::World::Path->first_match(
+        sub {
+            my $tile = shift;
+            $tile->isa('TAEB::World::Tile::Door');
+        }
+    );
+    $self->if_path($path => "Moving to the door");
+    return if defined($path) && length($path->path) != 0;
+
+    # look for a tile with a door nearby
+    $path = TAEB::World::Path->first_match(
+        sub {
+            my $tile = shift;
+            return 0 if $tile == TAEB->current_tile;
+            return $tile->any_adjacent(sub {
+                $_->isa('TAEB::World::Tile::Door');
+            });
+        }
+    );
+    $self->if_path($path => "Moving near the door");
+    return if defined($path) && length($path->path) != 0;
+
+    # look for a tile with a shopkeeper nearby
+    $path = TAEB::World::Path->first_match(
         sub {
             my $tile = shift;
             return 0 if $tile == TAEB->current_tile;
